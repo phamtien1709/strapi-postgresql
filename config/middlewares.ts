50eb72af892ea8211d2a2a25ec00a757962b0909
Titus Pham <phamtien1709@gmail.com>
2024-02-20 16:59:27 +0700

    Add CORS middleware

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,15 +2,8 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:1337', 'http://localhost:5173']
-    }
-  },
   'strapi::query',
   'strapi::body',
   'strapi::session',
